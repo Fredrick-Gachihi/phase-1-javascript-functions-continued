93855afdbf55a82a545d9d1b80dd42df4cfe6e43
fredrick-gachihi <fredrickgachihi24@gmail.com>
2024-04-03 09:32:40 +0300

    My functions continued

--- index.js
+++ index.js
@@ -2,11 +2,19 @@
 function saturdayFun(activity = 'roller-skate'){
     return `This Saturday, I want to ${activity}!`;
 }
+
 function mondayWork(activity = 'go to the office'){
     return `This Monday, I will ${activity}.`
    
 }
+
 function wrapAdjective(flair = '*'){
-    return function(adjective){
+    return function(adjective ='Special'){
     return `You are ${flair}${adjective}${flair}!`;
- }}
+    }
+}
+const encouragingPromptFunction1 = wrapAdjective("*")
+console.log(encouragingPromptFunction1("a hardworking student"));
+
+const encouragingPromptFunction2 = wrapAdjective("!!!")
+console.log(encouragingPromptFunction2("a hardworking student"));
